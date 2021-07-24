dc3eb715ebeae784551f59edf626f408098e6a6d
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-07-23 17:20:03 -0700

    Time: 84 ms (86.80%), Space: 42.7 MB (81.57%) - LeetHub

---
+++ jump-game/jump-game.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let lastPosition = nums.length - 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] + i >= lastPosition) {
+            lastPosition = i;
+        }
+    }
+    return lastPosition === 0;
+};
