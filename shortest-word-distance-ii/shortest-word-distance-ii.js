c68769c08e6bb46cd46dba3895afbb5c9bbff78b
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-12 19:32:31 -0700

    Time: 136 ms (48.45%), Space: 52.2 MB (6.70%) - LeetHub

--- shortest-word-distance-ii/shortest-word-distance-ii.js
+++ shortest-word-distance-ii/shortest-word-distance-ii.js
@@ -2,14 +2,13 @@
  * @param {string[]} wordsDict
  */
 var WordDistance = function(wordsDict) {
-    this.l = wordsDict.length;
-    this.storage = {};
+    this.map = {};
     for (let i = 0; i < wordsDict.length; i++) {
-        if (this.storage[wordsDict[i]]) {
-            this.storage[wordsDict[i]].push(i);
-        } else {
-           this.storage[wordsDict[i]] = [i]; 
-        }  
+        let word = wordsDict[i];
+        if (!this.map[word]) {
+            this.map[word] = [];
+        }
+        this.map[word].push(i);
     }
 };
 
@@ -19,19 +18,17 @@ var WordDistance = function(wordsDict) {
  * @return {number}
  */
 WordDistance.prototype.shortest = function(word1, word2) {
-    let min = this.l;
-    let pos1 = this.storage[word1];
-    let pos2 = this.storage[word2];
-    for (let i = 0, j = 0; i< pos1.length && j< pos2.length;) {
-       if (pos1[i] < pos2[j]) {
-           min = Math.min(min, pos2[j] - pos1[i]);
-           i++;
-       } else {
-          min = Math.min(min, pos1[i] - pos2[j]);
-           j++;
-       }
+    let order1 = this.map[word1].sort((a, b) => a- b);
+    let order2 = this.map[word2].sort((a, b) => a- b);
+    let res = Math.abs(order1[0] - order2[0]);
+    let i = 0, j = 0;
+    while ( i < order1.length && j < order2.length) {
+        res = Math.min(Math.abs(order1[i] - order2[j]), res);
+        if ( order1[i] < order2[j]) {
+            i++;
+        } else {j++}
     }
-    return min;
+    return res;
 };
 
 /** 
