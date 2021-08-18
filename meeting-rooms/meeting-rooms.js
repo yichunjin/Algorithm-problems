3a1a686f1bd896bce1b3f66bb2199594c779948e
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-18 15:48:10 -0700

    Time: 80 ms (71.86%), Space: 40.7 MB (64.37%) - LeetHub

--- meeting-rooms/meeting-rooms.js
+++ meeting-rooms/meeting-rooms.js
@@ -4,11 +4,8 @@
  */
 var canAttendMeetings = function(intervals) {
     intervals.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i< intervals.length; i++) {
-        if (intervals[i + 1] === undefined) {
-            return true;
-        }
-        if (intervals[i][1] > intervals[i + 1][0]) {
+    for (let i = 0; i < intervals.length - 1; i++) {
+        if ( intervals[i + 1][0] < intervals[i][1]) {
             return false;
         }
     }
