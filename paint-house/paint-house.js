0500496453bc6f4318838d1b608e3f4a18f74f4b
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-19 11:05:19 -0700

    Time: 76 ms (82.31%), Space: 43.2 MB (13.85%) - LeetHub

--- paint-house/paint-house.js
+++ paint-house/paint-house.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var minCost = function(costs) {
-    let cost = 0;
-    let memo = {};
     if (costs.length === 0) {
-        return 0;
+        return;
     }
+    let memo = {};
+    
     const paintCost = (n, color) => {
         if (memo[n + ',' + color]) {
             return memo[n + ',' + color];
@@ -15,14 +15,15 @@ var minCost = function(costs) {
         let totalCost = costs[n][color];
         if (n === costs.length - 1) {
         } else if (color === 0) {
-            totalCost += Math.min(paintCost(n + 1, 1), paintCost(n + 1, 2));
+            totalCost += Math.min(paintCost(n + 1, 1), paintCost(n + 1, 2))
         } else if (color === 1) {
-            totalCost += Math.min(paintCost(n + 1, 0), paintCost(n + 1, 2));
-        } else {
-            totalCost += Math.min(paintCost(n + 1, 1), paintCost(n + 1, 0));
+            totalCost += Math.min(paintCost(n + 1, 0), paintCost(n + 1, 2))
+        } else if (color === 2) {
+            totalCost += Math.min(paintCost(n + 1, 1), paintCost(n + 1, 0))
         }
         memo[n + ',' + color] = totalCost;
         return totalCost;
     }
-    return Math.min(paintCost(0, 0), paintCost(0, 1), paintCost(0, 2))
+    
+    return Math.min(paintCost(0, 0), paintCost(0, 1), paintCost(0, 2));
 };
