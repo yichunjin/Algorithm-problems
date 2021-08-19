3d63107efa81e0091c311669cbf6b1452f86a272
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-19 10:34:22 -0700

    Time: 80 ms (47.76%), Space: 40.5 MB (73.07%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
@@ -14,32 +14,34 @@ var zigzagLevelOrder = function(root) {
     if (!root) {
         return [];
     }
-    var res = [];
-    var q = [root, null];
-    var level_list = [];
-    var order = true;
+    let res = [];
+    let q = [root, null];
+    let levelList = [];
+    let order = true;
     while (q.length > 0) {
        let cur = q.shift();
         if (cur) {
             if (order) {
-                level_list.push(cur.val);
+                levelList.push(cur.val);
             } else {
-                level_list.unshift(cur.val)
+                levelList.unshift(cur.val);
             }
             if (cur.left) {
-                q.push(cur.left)
+                q.push(cur.left);
             }
             if (cur.right) {
                 q.push(cur.right)
             }
         } else {
-            res.push(level_list);
+            res.push(levelList);
+            levelList = [];
             if (q.length > 0) {
-                q.push(null);
+              q.push(null);  
             }
-            level_list = [];
-            order = !order
+            order = !order;
         }
+
+        
     }
     return res;
 };
