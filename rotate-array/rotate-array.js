93febbaac8660baf27d683e46e355cfc886eec77
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-25 16:29:15 -0700

    Time: 116 ms (41.63%), Space: 49.6 MB (28.99%) - LeetHub

---
+++ rotate-array/rotate-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const arr = new Array(nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        arr[(i + k) % nums.length] = nums[i];
+    }
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = arr[i];
+    }
+};
