8b0ecc92e7baaa87205e2d331aa2f6529290e30e
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-31 15:45:02 -0700

    Time: 117 ms (19.18%), Space: 40 MB (98.86%) - LeetHub

--- merge-intervals/merge-intervals.js
+++ merge-intervals/merge-intervals.js
@@ -3,12 +3,15 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    intervals.sort((a, b) => a[0] - b[0])
-  for (let i = 0; i < intervals.length - 1; i++) {
-      while (i < intervals.length - 1 && intervals[i][1] >= intervals[i + 1][0]) {
-          intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[i + 1][1])];
-          intervals.splice(i+1, 1);
-      }
-  }
-  return intervals;
+    intervals.sort((a, b) => a[0] - b[0]);
+    let i = 0;
+    while (i < intervals.length - 1) {
+        if (intervals[i + 1][0] <= intervals[i][1]) {
+            intervals[i][1] = Math.max(intervals[i + 1][1], intervals[i][1]);
+            intervals.splice(i + 1, 1);
+            i--;
+        }
+        i++;
+    }
+    return intervals;
 };
