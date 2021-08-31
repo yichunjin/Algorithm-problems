4f3ea2f2a3696f4a132a87409e4f14e426d0aa8d
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-31 11:49:27 -0700

    Time: 112 ms (10.98%), Space: 39.6 MB (96.53%) - LeetHub

---
+++ partition-list/partition-list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    if (!head) {
+        return head;
+    }
+    let small = new ListNode(0), large = new ListNode(0);
+    let sHead = small;
+    let lHead = large;
+    while (head) {
+        if (head.val < x) {
+            small.next = head;
+            small = small.next;
+        } else {
+            large.next = head;
+            large = large.next;
+        }
+        head = head.next;
+    }
+    large.next = null;
+    small.next = lHead.next;
+    return sHead.next;
+    
+};
