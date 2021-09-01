8caae42ddc468dace44fcd8035d647a3a393dc4a
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-08-31 17:27:21 -0700

    Time: 87 ms (57.31%), Space: 40.7 MB (92.69%) - LeetHub

--- insert-interval/insert-interval.js
+++ insert-interval/insert-interval.js
@@ -5,31 +5,28 @@
  */
 var insert = function(intervals, newInterval) {
     let inserted = [];
+    let pushed = false;
     if (intervals.length === 0) {
-        inserted.push(newInterval)
+        return [newInterval]
     }
-    for (let i = 0; i < intervals.length; i++) {
+    let i;
+    for (i = 0; i < intervals.length; i++) {
         if (newInterval[0] > intervals[i][1]) {
             inserted.push(intervals[i]);
-            if (i + 1 === intervals.length || newInterval[1] < intervals[i + 1][0]) {
-                inserted.push(newInterval);
-            }
-        } else if (intervals[i][0] > newInterval[1]) {
-            if (inserted.length === 0) {
-                inserted.push(newInterval)
-            }
-            inserted.push(intervals[i]);
+        } else if (newInterval[1] < intervals[i][0]) {
+            inserted.push(newInterval);
+            pushed = true;
+            break;
         } else {
-            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-            if (newInterval[1] <= intervals[i][1]) {
-                newInterval[1] = intervals[i][1];
-                inserted.push(newInterval);
-            } else {
-                if (i + 1 === intervals.length || newInterval[1] < intervals[i + 1][0]) {
-                    inserted.push(newInterval)
-                }
-            }
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
         }
+    }
+
+    if (!pushed) {
+        inserted.push(newInterval);
+    } else {
+        inserted = inserted.concat(intervals.slice(i));
     }
     return inserted;
 };
