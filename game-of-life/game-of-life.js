30e32328a3e7b09c3e1bc47b20e08beed3c3ea5e
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-02 15:40:12 -0700

    Time: 72 ms (82.75%), Space: 40.6 MB (13.97%) - LeetHub

--- game-of-life/game-of-life.js
+++ game-of-life/game-of-life.js
@@ -3,50 +3,42 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
-    const nei = [[1,0], [0,1],[-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]];
-    
-    const checkStatus = (i, j) => {
-        // change to 2 if from 0 > 1
-        // change to -1 if from 1 < 0
-        let zero = 0;
-        let one = 0;
-        for (let n of nei) {
-            let r = i + n[0];
-            let c = j + n[1];
-            if (r < 0 || c < 0 || r >= board.length || c >= board[r].length) {
-                continue;
-            }
-            if (board[r][c] === 1 || board[r][c] === -1) {
-                one++
-            } else {
-                zero++
-            }
-        }
-        if (board[i][j] === 0 && one === 3) {
-            return 2;
-        }
-        if (board[i][j] === 1) {
-            if (one < 2 || one > 3) {
-                return -1;
-            }
-        }
-        return board[i][j]
-    }
-    
+    const directions = [[0, 1],[0, -1],[1, 0],[-1, 0],[1, 1],[-1, 1],[1, -1],[-1, -1]];
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            board[i][j] = checkStatus(i, j);
+            // 0 >> 1 change to 2
+            // 1 >> 0 change to -1
+            let cur = board[i][j];
+            let one = 0;
+            for (let d of directions) {
+                let r = i + d[0], c = j + d[1];
+                if (r < 0 || c < 0 || r >= board.length || c >= board[r].length) {
+                    continue;
+                }
+                if (board[r][c] === 1 || board[r][c] === -1) {
+                    one++;
+                }
+            }
+            if (cur === 1) {
+                if (one < 2 || one > 3) {
+                    board[i][j] = -1;
+                }
+            } else if (cur === 0) {
+                if (one === 3) {
+                    board[i][j] = 2;
+                }
+            }
         }
     }
-    
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === -1) {
-                board[i][j] = 0
-            } else if (board[i][j] === 2) {
+                board[i][j] = 0;
+            }
+            if (board[i][j] === 2) {
                 board[i][j] = 1;
             }
         }
     }
-    return board
+    return board;
 };
