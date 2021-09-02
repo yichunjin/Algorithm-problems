a8fc8de803724019918ca47560c3c8729e76a757
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-02 11:38:50 -0700

    Time: 106 ms (23.34%), Space: 39.7 MB (94.49%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let prev = nums[0];
-    let res = nums[0];
+    let max = nums[0], prev = nums[0];
     for (let i = 1; i < nums.length; i++) {
-        let max = Math.max(prev + nums[i], nums[i]);
-        prev = max;
-        res = Math.max(res, prev);
+        let cur = Math.max(nums[i], nums[i] + prev);
+        max = Math.max(cur, max);
+        prev = cur;
     }
-    return res;
+    return max;
 };
