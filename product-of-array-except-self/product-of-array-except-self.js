dd5c461926d49d14564c49a23b087015474b258c
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-02 11:48:40 -0700

    Time: 180 ms (18.15%), Space: 50.8 MB (53.89%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.js
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -3,14 +3,13 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let result = new Array(nums.length).fill(1);
-    let prefix = 1;
+    const result = new Array(nums.length).fill(1);
+    let prefix = 1, postfix = 1;
     for (let i = 0; i < nums.length; i++) {
         result[i] = prefix;
         prefix *= nums[i];
     }
-    let postfix = 1;
-    for (let i = nums.length - 1; i > -1; i--) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         result[i] *= postfix;
         postfix *= nums[i];
     }
