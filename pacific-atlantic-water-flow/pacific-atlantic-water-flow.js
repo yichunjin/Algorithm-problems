ecf57d273015d20665bdbf5fc53ebfabac595442
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-17 10:43:29 -0700

    Time: 201 ms (37.93%), Space: 47.8 MB (55.17%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.js
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.js
@@ -3,38 +3,38 @@
  * @return {number[][]}
  */
 var pacificAtlantic = function(heights) {
-  let results = [];
-  let pac = new Set();
-  let atl = new Set();
+    const result = [];
+    const pac = new Set(), alt = new Set();
+    const r = heights.length, c = heights[0].length;
+    
+    const dfs = (i, j, visited, height) => {
+        if (i < 0 || j < 0 || i >= r || j >= c || visited.has(i + ',' + j) || heights[i][j] < height) {
+            return;
+        }
+        visited.add(i + ',' + j);
+        dfs(i + 1, j, visited, heights[i][j]);
+        dfs(i - 1, j, visited, heights[i][j]);
+        dfs(i, j + 1, visited, heights[i][j]);
+        dfs(i, j - 1, visited, heights[i][j]);
+    }
+    
+    for (let j = 0; j < c; j++) {
+        dfs(0, j, pac, 0);
+        dfs(r - 1, j, alt, 0);
+    }
+    
+    for (let i = 0; i < r; i++) {
+        dfs(i, 0, pac, 0);
+        dfs(i, c - 1, alt, 0);
+    }
+    
+    for (let i = 0; i < r; i++) {
+        for (let j = 0; j < c; j++) {
+            if (pac.has(i + ',' + j) && alt.has(i + ',' + j)) {
+                result.push([i, j]);
+            }
+        }
+    }
 
-  const dfs = (i, j, visit, height) => {
-      if (visit.has(i +','+ j) || i < 0 || j < 0 || i >= heights.length || j >= heights[i].length || heights[i][j] < height) {
-          return;
-      }
-      visit.add(i +','+ j);
-      dfs(i + 1, j, visit, heights[i][j])
-      dfs(i - 1, j, visit, heights[i][j])
-      dfs(i, j + 1, visit, heights[i][j])
-      dfs(i, j - 1, visit, heights[i][j])
-  }
-
-  for (let j = 0; j < heights[0].length; j++) {
-      dfs(0, j, pac, heights[0][j])
-      dfs(heights.length - 1, j, atl, heights[heights.length - 1][j])
-  }
-
-  for (let i = 0; i < heights.length; i++) {
-      dfs(i, 0, pac, heights[i][0])
-      dfs(i, heights[0].length - 1, atl, heights[i][heights[0].length - 1])
-  }
-
-  for (let i = 0; i < heights.length; i++) {
-      for (let j = 0; j < heights[i].length; j++) {
-          if (pac.has(i +','+ j) && atl.has(i +','+ j)) {
-              results.push([i, j]);
-          }
-      }
-  }
-
-  return results;
+    return result;
 };
