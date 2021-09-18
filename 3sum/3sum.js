b776dd36293acdb3184bc1a3e0b4496ad4128209
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-18 16:33:17 -0700

    Time: 384 ms (30.32%), Space: 48 MB (98.71%) - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -3,31 +3,29 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  let results = [];
-  nums.sort((a, b) => {return a - b});
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || i > 0 && (nums[i] !== nums[i - 1])) {
-      let start = i + 1;
-      let end = nums.length - 1;
-      let sum = 0 - nums[i];
-      while (start < end) {
-          if (nums[start] + nums[end] === sum) {
-              results.push([nums[i], nums[start], nums[end]]);
-              while (start < end && nums[start] === nums[start + 1]) {
-                  start++;
-              }
-              while (start < end && nums[end] === nums[end - 1]) {
-                  end--;
-              }
-              start++;
-              end--;
-          } else if (nums[start] + nums[end] > sum) {
-              end--;
-          } else if (nums[start] + nums[end] < sum) {
-              start++;
-          }
-      }
+    const result = [];
+    
+    const twoSum = (i) => {
+        const seen = new Set();
+        for (let j = i + 1; j < nums.length; j++) {
+            let positive = 0 - nums[i] - nums[j];
+            if (seen.has(positive)) {
+                result.push([nums[i], nums[j], positive]);
+                while (j + 1 < nums.length && nums[j] === nums[j + 1]) {
+                    j++;
+                }
+            }
+            seen.add(nums[j]);
+        }
     }
-  }
-  return results;
+    
+    nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length && nums[i] <= 0; i++) {
+        if (i === 0 || nums[i - 1] !== nums[i]) {
+            twoSum(i);
+        }
+    }
+    
+    return result;
 };
