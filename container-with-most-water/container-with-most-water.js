eb555b2f531e0f72669e6a9f3785c131c15b1a6e
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-19 13:29:08 -0700

    Time: 130 ms (24.69%), Space: 47.9 MB (70.25%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -2,20 +2,19 @@
  * @param {number[]} height
  * @return {number}
  */
-// testcases:
-// [1, 1], [9, 9], [0, 9], [1,2,3,4,5,6,7,8,9]
 var maxArea = function(height) {
-    let max = 0;
-    let i = 0; 
-    let j = height.length - 1;
-    while ( i < j) {
-        let water = Math.min(height[i], height[j]) * (j - i);
-        max = Math.max(water, max);
-        if (height[i] > height[j]) {
-            j = j - 1;
+    let left = 0, right = height.length - 1;
+    let result = 0;
+    
+    while (left < right) {
+        let curArea = Math.min(height[left], height[right]) * (right - left);
+        result = Math.max(result, curArea);
+        if (height[left] < height[right]) {
+            left++;
         } else {
-            i = i + 1;
+            right--;
         }
     }
-    return max;
+    
+    return result;
 };
