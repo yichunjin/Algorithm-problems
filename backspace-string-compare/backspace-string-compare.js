2ce703b550766e4f496b1c9c1079ab0b100b8d9b
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-18 17:47:06 -0700

    Time: 72 ms (85.57%), Space: 38.9 MB (75.52%) - LeetHub

---
+++ backspace-string-compare/backspace-string-compare.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    let i = s.length - 1, j = t.length - 1;
+    let skipS = 0, skipT = 0;
+    while (i >= 0 || j >= 0) {
+        while (i >= 0) {
+            if (s[i] === '#') {
+                skipS++; i--;
+            } else if (skipS > 0) {
+                skipS--; i--;
+            } else {
+                break;
+            }
+        }
+        
+        while (j >= 0) {
+            if (t[j] === '#') {
+                skipT++; j--;
+            } else if (skipT > 0) {
+                skipT--; j--;
+            } else {
+                break;
+            }
+        }
+        
+        if (s[i] !== t[j]) {
+            return false;
+        }
+        
+        if ( (i >= 0) !== (j >= 0)) {
+            return false;
+        }
+        i--; j--;
+    }
+    return true;
+};
