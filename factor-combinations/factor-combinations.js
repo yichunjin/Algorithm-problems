a63a75818915bf7d5fe8a687bde3a56dbe635131
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-23 10:50:29 -0700

    Time: 92 ms (100.00%), Space: 46.7 MB (7.58%) - LeetHub

--- factor-combinations/factor-combinations.js
+++ factor-combinations/factor-combinations.js
@@ -4,13 +4,14 @@
  */
 var getFactors = function(n, si = 2) {
     let res = [];
-    for (let i = si; i * i <= n; i++) {
+    for (let i = si; i*i <= n; i++) {
         if (n % i === 0) {
             let next = n / i;
             res.push([i, next]);
+            
             for (let arr of getFactors(next, i)) {
-              res.push([i, ...arr]);  
-            }        
+                res.push([i, ...arr]);
+            }
         }
     }
     return res;
