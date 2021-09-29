570bd256c8ab16e210a7a9c13a47708721e3cf8b
yichunjin <50721156+yichunjin@users.noreply.github.com>
2021-09-29 14:26:31 -0700

    Time: 76 ms (99.03%), Space: 41.7 MB (67.88%) - LeetHub

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -3,24 +3,28 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    const result = [];
-    const dfs = (cur, used) => {
+    const results = [];
+    const used = new Array(nums.length).fill(false);
+    
+    const dfs = (cur) => {
         if (cur.length === nums.length) {
-            result.push(cur.slice());
+            results.push(cur.slice());
             return;
         }
+        
         for (let i = 0; i < nums.length; i++) {
             if (used[i]) {
                 continue;
             }
             cur.push(nums[i]);
             used[i] = true;
-            dfs(cur, used);
+            dfs(cur);
             cur.pop();
             used[i] = false;
         }
     }
-    let used = new Array(nums.length).fill(false);
-    dfs([], used);
-    return result;
+    
+    dfs([]);
+    
+    return results;
 };
